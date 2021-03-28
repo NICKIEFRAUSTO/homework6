f6be1fc828190f190f49bd90ff65b1de30edaeff
Nickie Frausto <79732055+NICKIEFRAUSTO@users.noreply.github.com>
2021-03-27 21:54:58 -0500

    added api key

--- assets/script.js
+++ assets/script.js
@@ -2,14 +2,26 @@ var requestUrl = 'https://api.openweathermap.org';
 var fetchButton = document.getElementById('fetch-button');
 
 
-fetch(requestUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    Storage.local()
-    for (var i = 0; i < data.length; i++) {
-      console.log(data[i].url);
+
+// getApi(requestUrl);
+let city = "dallas"
+let apiKey = "d466d46c54bd0adeec8e8694c4da4560"
+fetch("https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid="+apiKey)
+.then(response => response.json())
+.then(function(data){
+    console.log(data);
+})
+
+// fetchButton.addEventListener('click', getApi);
+
+// fetch(requestUrl)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     Storage.local()
+//     for (var i = 0; i < data.length; i++) {
+//       console.log(data[i].url);
       
-    }
-  });
+//     }
+//   });
