25d0dd4c64d3e6aae809e548c9e513226255f2d4
Nickie Frausto <79732055+NICKIEFRAUSTO@users.noreply.github.com>
2021-03-29 19:48:13 -0500

    updated form and removed city buttons

--- assets/script.js
+++ assets/script.js
@@ -2,12 +2,15 @@ var requestUrl = 'https://api.openweathermap.org';
 var fetchButton = document.getElementById('fetch-button');
 
 
-
 // getApi(requestUrl);
+
 let city = "dallas"
 let apiKey = "d466d46c54bd0adeec8e8694c4da4560"
 fetch("https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid="+apiKey)
-.then(response => response.json())
+
+.then(response => 
+    response.json())
+
 .then(function(data){
     console.log(data);
 })
